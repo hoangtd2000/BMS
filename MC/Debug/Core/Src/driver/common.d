5dd1adfb0b11f270ea9788457b57dafe0cf828c6
hoang <hoang.rd.vn@mcnex.com>
2026-07-17 15:39:03 +0700

    add: thêm file gitignore

--- MC/Debug/Core/Src/driver/common.d
+++
@@ -1,3 +0,0 @@
-Core/Src/driver/common.o: ../Core/Src/driver/common.c \
- C:/Users/Admin/Desktop/BMS/MC/Core/Inc/driver/common.h
-C:/Users/Admin/Desktop/BMS/MC/Core/Inc/driver/common.h:
--- MC/Debug/Core/Src/driver/tick.d
+++
@@ -1,3 +0,0 @@
-Core/Src/driver/tick.o: ../Core/Src/driver/tick.c \
- C:/Users/Admin/Desktop/BMS/MC/Core/Inc/driver/tick.h
-C:/Users/Admin/Desktop/BMS/MC/Core/Inc/driver/tick.h:
